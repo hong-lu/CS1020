828fbb57fb451877bf069d2e965d1e4b44961078
Hong Lu <hlmontgomory@gmail.com>
2015-02-07 14:17:57 +0800

    Completed ex2

--- lab2_2/skeleton/SocialNetwork.java
+++ lab2_2/skeleton/SocialNetwork.java
@@ -1,8 +1,8 @@
 // CS1020 (AY2014/5 Semester 2)
 // Take-home Lab2 Ex2
-// Name: 
-// Matric. No.: 
-// Lab group: 
+// Name: Hong Lu
+// Matric. No.: A0133889M
+// Lab group: 05
 // Write the program description below.
 // It is mandatory to write program description at the top of every program.
 // Marks will be awarded for this in sit-in labs.
@@ -11,50 +11,90 @@
 // Add import statement(s) below
 import java.util.*;
 
-
 public class SocialNetwork {
-	//Define your data attributes here
+ //Define your data attributes here
+  private User[] usersList;
+  private int numOfTotalUsers = 0;
+
+ //constructor
+ public SocialNetwork() {
+  this.usersList = new User[100];
+ }
+
+ //return the number of pairs of mutual follow friends
+ public int findMutualFollow() {
+  int numOfMutualFollow = 0;
+  
+  //write your solution here
+   for(int i = 0; i < this.numOfTotalUsers - 1; i++){
+     for(int j = i + 1; j < this.numOfTotalUsers; j++){
+       if (usersList[i].isFollowing(usersList[j]) && usersList[j].isFollowing(usersList[i])){
+         numOfMutualFollow ++;
+       }
+     }
+   }
+  return numOfMutualFollow;
+ }
+
+ //register a new user with a given name
+ public void registerUser(String name) {
+  this.usersList[this.numOfTotalUsers] = new User(name);
+  this.numOfTotalUsers++;
+ }
+
+ //set user A follows user B
+ public void setFollow(User A, User B) {
+  A.setFollow(B);
+ }
+ 
+ //set user A unfollows user B
+ public void setUnfollow(User A, User B) {
+  A.setUnfollow(B);
+ }
+
+ //find the user object according to the name given
+ public User getUserByName(String name){
+   User result = new User("");
+   for (User u: this.usersList){
+     if( u.getName().equals(name)){
+       result = u;
+       break;
+     }
+   }
+   return result;
+ }
 
 
-	//constructor
-	public SocialNetwork() {
-		
-	}
-
-	//return the number of pairs of mutual follow friends
-	public int findMutualFollow() {
-		int numOfMutualFollow = 0;
-		
-		//write your solution here
-
-		return numOfMutualFollow;
-	}
-
-	//register a new user with a given name
-	public void registerUser(String name) {
-		
-	}
-
-	//set user A follows user B
-	public void setFollow(User A, User B) {
-		
-	}
-	
-	//set user A unfollows user B
-	public void setUnfollow(User A, User B) {
-		
-	}
-
-
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		SocialNetwork socialNetwork = new SocialNetwork();
-		
-		//write your code here
-		System.out.println("Number of pairs of mutual followers: " 
-		                    + socialNetwork.findMutualFollow());
-		
-	}
+ public static void main(String[] args) {
+  Scanner sc = new Scanner(System.in);
+  SocialNetwork socialNetwork = new SocialNetwork();
+  
+  //write your code here
+  while(sc.hasNext()){
+    String str = sc.nextLine();
+    String[] operation = str.split(" ");
+    String command = operation[0];
+    if (command.equals("R")){
+      for (int i = 1; i < operation.length; i++){
+        socialNetwork.registerUser(operation[i]);
+      }
+    }
+    else if (command.equals("F") ){
+        User A = socialNetwork.getUserByName(operation[1]);
+        User B = socialNetwork.getUserByName(operation[2]);
+        socialNetwork.setFollow(A, B);
+    }
+    else if (command.equals("U")){
+        User A = socialNetwork.getUserByName(operation[1]);
+        User B = socialNetwork.getUserByName(operation[2]);
+        socialNetwork.setUnfollow(A, B);
+    }
+    else if (command.equals("M")){
+        System.out.println("Number of pairs of mutual followers: " 
+                      + socialNetwork.findMutualFollow());
+    }
+  }
+  
+ }
 
 }
--- lab2_2/skeleton/User.java
+++ lab2_2/skeleton/User.java
@@ -1,8 +1,8 @@
 // CS1020 (AY2014/5 Semester 2)
 // Take-home Lab2 Ex2
-// Name: 
-// Matric. No.: 
-// Lab group: 
+// Name: Hong Lu
+// Matric. No.: A0133889M
+// Lab group: 05
 // Write the program description below.
 // It is mandatory to write program description at the top of every program.
 // Marks will be awarded for this in sit-in labs.
@@ -12,25 +12,58 @@
 import java.util.*;
 
 public class User {
-	//Define your data attributes here
-	
+ //Define your data attributes here
+ private String name;
+ private User[] followUsers;
+ //keep track of the number of users that current user is following
+ private int numOfFollowing;
 
-	//constructor
-	public User(String Name) {
-		
-	}
-	
+ //constructor
+ //Assume there will not be more than 100 users
+ //Assume that no two same names will be registered
+ public User(String name) {
+  this.name = name;
+  this.followUsers = new User[100];
+  this.numOfFollowing = 0;
+ }
+ 
 
-	//set this user follows a given user. 
-	public void setFollow(User user) {
-		
-	}
-	
-	//set this user unfollows a given user.
-	public void setUnfollow(User user) {
-		
-	}
-	
-	//You can define your own methods
-	
+ //set this user follows a given user. 
+ public void setFollow(User user) {
+  //A user cannot follow himself or a user that he has already followed
+   if (user != this && (!this.isFollowing(user))){
+     this.followUsers[this.numOfFollowing] = user;
+     this.numOfFollowing++;
+   }
+ }
+ 
+ //set this user unfollows a given user.
+ public void setUnfollow(User user) {
+   User[] newFollowUsers = new User[100];
+   int i = 0;
+   for(User u : this.followUsers){
+     if (u != user){
+       newFollowUsers[i] = u;
+       i++;
+     }
+   }
+   this.numOfFollowing --;
+   this.followUsers = newFollowUsers;
+ }
+ 
+ 
+ //Check whether current user is following user A
+ public boolean isFollowing(User A){
+   for (int i = 0; i < numOfFollowing; i++){
+     if (followUsers[i].equals(A))
+       return true;
+   }
+   return false;
+ }
+ 
+ //Accessor
+ public String getName(){
+   return this.name;
+ }
+ 
 }
